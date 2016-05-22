191f13b166e30430d3ed9445321a066c89e5471c
edison <lrobalino@iesjoandaustria.org>
2016-05-23 01:15:26 +0200

    Exercici 16 (eliminada la variable total)

--- Client.java
+++ Client.java
@@ -56,7 +56,6 @@ public class Client {
     }
 
 	public String informe() {
-		double total = 0;
 		int bonificacions = 0;
 		String resultat = "Informe de lloguers del client " +
 			getNom() +
@@ -69,13 +68,20 @@ public class Client {
 				" " +
 				lloguer.getVehicle().getModel() + ": " +
 				lloguer.quantitat() + "€" + "\n";
-			total += lloguer.quantitat();
 		}
 
 		// afegeix informació final
-		resultat += "Import a pagar: " + total + "€\n" +
+		resultat += "Import a pagar: " + importTotal() + "€\n" +
 			"Punts guanyats: " + bonificacions + "\n";
 		return resultat;
+	}
+	
+	public double importTotal(){
+		double total = 0;
+		for (Lloguer lloguer: lloguers) {
+			total += lloguer.quantitat();
+		}
+		return total;
 	}
     
     public Vector<Lloguer> getLloguers(){
